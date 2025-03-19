84ffcf83b510a815a875e3b8c4d76a6f642b5ced
Bruno <bruno.portess@gmail.com>
2025-03-18 22:57:52 -0300

    Iniciar projeto react

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./style/custom.css";
+
+import AppRouter from "./routes";
+
+export default function App() {
+
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
+}
