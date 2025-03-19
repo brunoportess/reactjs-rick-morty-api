43ae3c05c5aca0eed18e0bfc82b4b8e3192b72e1
Bruno <bruno.portess@gmail.com>
2025-03-18 23:03:26 -0300

    paagina de detalhes

---
+++ src/pages/characterDetails.tsx
@@ -0,0 +1,23 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+
+
+
+const CharacterDetails = () => {
+
+  
+
+  return (
+    <Container className="mt-4">
+      <h2>Pesquisa de Personagens</h2>
+
+      <Row>
+        <Col>
+            Detalhes
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CharacterDetails;
